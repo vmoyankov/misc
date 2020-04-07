32bb560772b537db1686886e2310d59cc738828d
Venko Moyankov <venko@storpool.com>
2020-04-08 02:28:48 +0300

    sub_edit: --transform accepts 4 arguments
    --transform accepts 2 timestaps for the sub and movie - at the begining
    and at the end and calculates delay and stretch. The format is
      --transform sub_ts1 mov_ts1 sub_ts2 mov_ts2

--- sub_edit.py
+++ sub_edit.py
@@ -5,6 +5,7 @@ import sys
 import argparse
 import re
 from datetime import timedelta, datetime
+import codecs
 
 parser = argparse.ArgumentParser()
 parser.add_argument('filename',
@@ -13,21 +14,52 @@ parser.add_argument('-d', '--delay', type=float, default=0.0,
         help="add delay in seconds")
 parser.add_argument('-s', '--stretch', type=float, default=1.0,
         help="Stretch subtitles by factor s e.g. 1.001")
-parser.add_argument('-t', '--transform', nargs=2,
-        help="Transform HH:MM:SS -> HH:MM:SS. Overrides -s")
+parser.add_argument('-t', '--transform', nargs=4,
+        help="Stretch/shring/delay/advance. "
+        "Provide 4 timestamps sub1 mov1 sub2 mov2 "
+        "in the form HH:MM:SS[.mmm]. "
+        "mov1 and sub1 shall match the same moment "
+        "near the beggining of the movie and mov2 sub2 near the end. "
+        "This overrides -s and -d. ")
+
 args = parser.parse_args()
 
+def parse_time_with_ms(a):
+    try:
+        t = datetime.strptime(a, '%H:%M:%S') 
+    except ValueError:
+        t = datetime.strptime(a, '%H:%M:%S.%f') 
+    return t - datetime.strptime('00:00:00', '%H:%M:%S')
+
 if args.transform:
-    td1 = (datetime.strptime(args.transform[0], '%H:%M:%S') -
-            datetime.strptime('00:00:00', '%H:%M:%S'))
-    td2 = (datetime.strptime(args.transform[1], '%H:%M:%S') -
-            datetime.strptime('00:00:00', '%H:%M:%S'))
-    args.stretch = td2 / td1
+    ts1 = parse_time_with_ms(args.transform[0])
+    tm1 = parse_time_with_ms(args.transform[1])
+    ts2 = parse_time_with_ms(args.transform[2])
+    tm2 = parse_time_with_ms(args.transform[3])
+    mov_diff = tm2 - tm1
+    sub_diff = ts2 - ts1
+    args.stretch = mov_diff / sub_diff
+    s1 = ts1 * args.stretch
+    args.delay = (tm1 - s1).total_seconds()
+    print("Stretch = %f, delay=%f" % (args.stretch, args.delay),
+            file=sys.stderr)
 
 patt = re.compile(r'^(\d\d):(\d\d):(\d\d),(\d\d\d) --> '
         r'(\d\d):(\d\d):(\d\d),(\d\d\d)')
 
-with open(args.filename, encoding='cp1251') as f:
+encodings = ['utf-8', 'cp1251', 'ascii']
+for e in encodings:
+    try:
+        f = codecs.open(args.filename, 'r', encoding=e)
+        f.readlines(20)
+        f.seek(0)
+    except UnicodeDecodeError:
+        print("Bad codec %s" % e, file=sys.stderr)
+    else:
+        print("File opened with %s encoding" % e, file=sys.stderr)
+        break
+
+with f:
     for l in f:
         m = patt.search(l)
         if m:
@@ -39,16 +71,14 @@ with open(args.filename, encoding='cp1251') as f:
             m2 = int(m.group(6))
             s2 = int(m.group(7))
             x2 = int(m.group(8))
-            t1 = timedelta(hours=h1, minutes=m1, seconds=s1, milliseconds=x1)
-            t2 = timedelta(hours=h2, minutes=m2, seconds=s2, milliseconds=x2)
-            y1 = t1 * args.stretch + timedelta(seconds=args.delay)
-            y2 = t2 * args.stretch + timedelta(seconds=args.delay)
-            s1 = y1.seconds + y1.microseconds / 1e6
-            s2 = y2.seconds + y1.microseconds / 1e6
-            h1, s1 = divmod(s1, 3600)
+            t1 = timedelta(hours=h1, minutes=m1, seconds=s1, milliseconds=x1).total_seconds()
+            t2 = timedelta(hours=h2, minutes=m2, seconds=s2, milliseconds=x2).total_seconds()
+            y1 = t1 * args.stretch + args.delay
+            y2 = t2 * args.stretch + args.delay
+            h1, s1 = divmod(y1, 3600)
             m1, s1 = divmod(s1, 60)
             s1, x1 = divmod(s1, 1)
-            h2, s2 = divmod(s2, 3600)
+            h2, s2 = divmod(y2, 3600)
             m2, s2 = divmod(s2, 60)
             s2, x2 = divmod(s2, 1)
             print("{h1:02.0f}:{m1:02.0f}:{s1:02.0f},{x1:03.0f} --> "
