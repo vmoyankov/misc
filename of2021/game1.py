debcc773cdca113e4c682048a05a196bf95be595
Venko Moyankov <venko@storpool.com>
2021-08-13 22:24:09 +0300

    A game for openfest

--- of2021/game1.py
+++ of2021/game1.py
@@ -7,28 +7,49 @@
 
 import hashlib
 import re
+import subprocess
 
 SECRET_CODE = "XXXX"  # It is replaced in the real application
 URL = "http://example.com"
 
-message = ("Здарвейте! Въведете вашето име (на латиница) и e-mail адрес за да "
-        "участвате в нашата лотария и да спечелите тениска. "
-        "Ако оставите на случайността, шансовете не са много големи, но "
-        "с малко съобразителност и знания може много да ги повишите. "
-        "Изходния код на програмата която определя печелившите можете да "
-        f"намерите тук {URL}. Успех!")
+
+message = f"""
+            Здравейте! Благодарим ви, че се включвате в играта организирана от
+            StorPool Storage за OpenFest 2021!
+
+            За да участвате, въведете реален e-mail адрес, на който ще получите
+            потвърждение за успешното решаване на задачата.
+
+            Не оставяйте нищо на случайността! С малко съобразителност и
+            хакерски умения, можете да повишите вашите шансове и да спечелите
+            свежа StorPool тениска.  Наградата може да получите на нашия щанд
+            на OpenFest 2021 или с куриер до ваш адрес.
+
+            Изходният код на програмата, с която можете да хакнете играта на
+            StorPool, ще намерите тук {URL}
+
+            Успех! Нека силата бъде с вас!
+"""
 
 def main():
     print(message)
 
-    name=''
-    email=''
+    name = None
+    email = None
 
     try:
-        while not re.match(r'[\w\- ]+$', name):
-            name = input("Име (на латиница): ")
-        while not re.match(r'[\w\-\.]+@[a-zA-z_.-]+$', email):
+        while True:
+            name = input("Име: ")
+            if re.match(r'[\w\- ]+$', name):
+                break
+            print("Невалиден формат.")
+
+        while True:
             email = input("e-mail адрес: ")
+            if re.match(r'[\w\-\.]+@[a-zA-z_.-]+$', email):
+                break
+            print("Невалиден формат.")
+
     except UnicodeDecodeError as e:
         print("Bad encoding:", e)
         return
@@ -42,13 +63,19 @@ def main():
 
     h = m.hexdigest()
     if h.startswith('00'):
-        print("Честито! Вие печелите.")
-        print("Покажете този код за да получите наградата: ", SECRET_CODE)
+        print("Честито! Вие печелите!")
+        print(f"Изпратихме Ви информация как да получите наградата си на посочения от Вас адрес {email}")
+        send_confirmation(email, name)
     else:
         print("Съжалявам, не печелите. Може да опитате пак с друго име "
                 "или e-mail адрес.")
 
 
+def send_confirmation(email, name):
+    try:
+        subprocess.run(["./send_message", email, name])
+    except subprocess.CalledProcessError as e:
+        print("Error sending email", e)
 
 if __name__ == '__main__':
     main()
