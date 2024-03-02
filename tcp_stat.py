97536e822a2180eea1e1a6c6ab0947d01dab6c9b
Peter Pentchev <roam@ringlet.net>
2024-03-02 02:21:56 +0200

    tcp_stat: reformat the source code using the Ruff formatter
    As an aside, move the copyright notice to a comment; it does not really
    belong in the module docstring.

--- tcp_stat.py
+++ tcp_stat.py
@@ -1,34 +1,31 @@
 #!/usr/bin/env python3
+# Copyright 2024 Venko Moyankov
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+"""Display the deltas in the output of the `ss` sockstat tool second by second."""
 
-"""
-Copyright 2024 Venko Moyankov
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-     http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-
-"""
-
+import argparse
 import subprocess
 import time
-import argparse
+
 
 def main():
-
-
+    """Parse command-line arguments, run `ss` repeatedly, process its output."""
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--filter', help='Filter sessions by address:port')
-    parser.add_argument('-F', '--params', help='Filter by param, coma separated list')
-    parser.add_argument('-v', '--vertical', action='store_true')
-    args=parser.parse_args()
+    parser.add_argument("-f", "--filter", help="Filter sessions by address:port")
+    parser.add_argument("-F", "--params", help="Filter by param, coma separated list")
+    parser.add_argument("-v", "--vertical", action="store_true")
+    args = parser.parse_args()
 
     if args.vertical:
         delimiter = "\n"
@@ -43,39 +40,39 @@ def main():
     sessions = {}
     while True:
         session = None
-        res = subprocess.run(['ss', '-tni'], stdout=subprocess.PIPE, encoding='utf-8').stdout
+        res = subprocess.run(["ss", "-tni"], stdout=subprocess.PIPE, encoding="utf-8").stdout
         ignore_data = True
         for line in res.splitlines():
             fields = line.split()
             if fields and line[0][0].isalpha():
-                if fields[0] == 'ESTAB':
-                    sid = '-'.join(fields[3:5])
+                if fields[0] == "ESTAB":
+                    sid = "-".join(fields[3:5])
                     if sid not in sessions:
-                        sessions[sid]={}
+                        sessions[sid] = {}
                     session = sessions[sid]
                     ignore_data = False
                 else:
                     ignore_data = True
-            elif not ignore_data and any(field.startswith('bytes_sent:') for field in fields):
+            elif not ignore_data and any(field.startswith("bytes_sent:") for field in fields):
                 if args.filter and args.filter not in sid:
                     continue
-                stats={}
+                stats = {}
                 for word in fields[1:]:
-                    if ':' in word:
-                        k,v = word.split(":", 1)
+                    if ":" in word:
+                        k, v = word.split(":", 1)
                         try:
                             stats[k] = int(v)
                         except ValueError:
                             pass
                 print(sid, end=delimiter)
-                for k,v in stats.items():
+                for k, v in stats.items():
                     if k not in session:
                         continue
                     if params and k not in params:
                         continue
                     d = v - session[k]
-                    print("{}: {:d}".format(k,d), end=delimiter)
-                sessions[sid]=stats
+                    print("{}: {:d}".format(k, d), end=delimiter)
+                sessions[sid] = stats
                 print()
 
         time.sleep(1)
